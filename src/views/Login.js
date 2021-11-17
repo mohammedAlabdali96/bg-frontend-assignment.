b3edfe6ec5337ac7f7482f037a919e78f34c628b
mohammedAlabdali96 <ddaf051@gmail.com>
2021-11-17 22:44:32 +0200

    add validation

--- src/views/Login.js
+++ src/views/Login.js
@@ -8,10 +8,12 @@ import UserContext from "../contexts/user.context";
 
 const Login = props => {
     const [id, setId] = useState("");
+    const [email, setEmail] = useState(true)
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
     const { user, setUser } = useContext(UserContext);
 
+    const emailRegex = /[a-z0-9]{2,8}@+([0-9]{4})\..*?[a-z]{2}$/;
     const handleLogin = (e) => {
         e.preventDefault();
 
@@ -27,6 +29,14 @@ const Login = props => {
             }
         )
     };
+    const handleChange = (ev) => {
+        setId(ev.target.value)
+        if (!ev.target.value.match(emailRegex)) {
+            setEmail(false)
+        }else {
+            setEmail(true)
+        }
+    }
 
     return (
         <div className="col-md-12">
@@ -45,9 +55,11 @@ const Login = props => {
                             placeholder="Colonist ID"
                             name="id"
                             value={id}
-                            onChange={(ev) => setId(ev.target.value)}
+                            onChange={(ev) => handleChange(ev, "id")}
                         />
                     </div>
+                    <div className="my-2">{!email && 'your eamil is not valid'}</div>
+
 
                     <div className="form-group">
                         <input
@@ -56,12 +68,14 @@ const Login = props => {
                             placeholder="Password"
                             required
                             name="password"
-                            value={password}
+                            autoComplete="off"
+                            autoCorrect="off"
+                            autoCapitalize="none" value={password}
                             onChange={(ev) => setPassword(ev.target.value)}
                         />
                     </div>
 
-                    <div className="form-group">
+                    <div className="form-group mt-4">
                         <button className="btn btn-primary btn-block" disabled={loading}>
                             {loading && (
                                 <span className="spinner-border spinner-border-sm"></span>
